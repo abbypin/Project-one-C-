27b99bd56bfbdc3fac26b601efca6450df2a0e41
abbypin <abby@techcleanup.com>
2022-11-05 12:19:59 -0700

    First commit
    "Rock, Paper, Scissors" first version

---
+++ rockPaperScissors.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    /* STEP 0. */
+    const int ROCK = 0;
+    const int PAPER = 1;
+    const int SCISSORS = 2;
+
+    int seed;
+    cin >> seed;
+    srand(seed);
+
+    /* Type your code here. */
+    /* STEP 1. */
+
+
+    return 0;
+}
