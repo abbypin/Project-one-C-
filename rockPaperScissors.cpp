0d8094f3dc3be73febef0c2fb746a71a003b4aef
abbypin <abby@techcleanup.com>
2022-11-05 12:49:50 -0700

    Step 1 Completed commit
    Lab2 step 1. Game Setup.

--- rockPaperScissors.cpp
+++ rockPaperScissors.cpp
@@ -1,19 +1,36 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    /* STEP 0. */
+    /* STEP 0. Variables */
     const int ROCK = 0;
     const int PAPER = 1;
     const int SCISSORS = 2;
+    /* STEP 1. Variables */
+    string name1;
+    string name2;
+    int rounds;
 
+    /* Step 0. */ 
     int seed;
     cin >> seed;
     srand(seed);
 
     /* Type your code here. */
     /* STEP 1. */
+    // Input Setup Game
+    cin >> name1;
+    cin >> name2;
+    cin >> rounds;
+    // Check Rounds
+    while (rounds < 0) {
+        cout << "Rounds must be > 0" << endl;
+        cin >> rounds;
+    }//end while-loop
 
+    // Print Beginning Message
+    cout << name1 << " vs " << name2 << " for " << rounds << " rounds" << endl;
 
     return 0;
 }
