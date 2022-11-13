dcef8ea9c7dafaf9e4db0a5bf9f7685adddaea27
abbypin <abby@techcleanup.com>
2022-11-13 11:39:33 -0800

    Finished Lab3-Step0
    Setup Vectors

--- creditCard.cpp
+++ creditCard.cpp
@@ -7,40 +7,47 @@ using namespace std;
 // Read customer information from external file
 // Make no changes to the following code
 void ReadCustomerData(vector<string>& names, vector<string>& states, vector<double>& debt) {
-   ifstream inFS;
+    // Create Variable
+    ifstream inFS;
 
-   // Read all data from file into three parallel vectors
-   try {
-      inFS.open("CustomerData.csv");
+    // Read all data from file into three parallel vectors
+    try {
+        // Open File
+        inFS.open("CustomerData.csv");
 
-      for (long unsigned index = 0; index < names.size(); ++index) {
-         inFS >> names.at(index); // last name
-         inFS >> states.at(index); // state of residence
-         inFS >> debt.at(index);   // amount of debt
-      }
-   }
-   // What if data file not found?
-   catch (string err) {
-      cout << "Failed to read the data file: " << err << endl;
-   }
+        // Add Data From File
+        for (long unsigned index = 0; index < names.size(); ++index) {
+            inFS >> names.at(index); // last name
+            inFS >> states.at(index); // state of residence
+            inFS >> debt.at(index);   // amount of debt
+        }
+    } catch (string err) {
+        // File Not Found
+        cout << "Failed to read the data file: " << err << endl;
+    }//end try-catch
 
-   inFS.close();
+    // Close File
+    inFS.close();
 }//end ReadCustomerData()
 
 int main() {
     int numOfCustomers = 0;
     // Input # of customers and create parallel vectors
+    /* Type your code */
+    
+    // Get Requested Number of Customers
     cout << "How many customers are there?" << endl;
     cin >> numOfCustomers;
+    // Create Parallel Vectors
     vector<string> names(numOfCustomers);
     vector<string> states(numOfCustomers);
     vector<double> debt(numOfCustomers);
-    ReadCustomerData(names, states, debt);
-
-
 
     // Fill vectors with data from another external file
-    /* Type your code here */      
+    /* Type your code here */  
+    // Read File Data into the Vectors
+    ReadCustomerData(names, states, debt);
+
    // Input debt limit, search phrase, and state
 
 
