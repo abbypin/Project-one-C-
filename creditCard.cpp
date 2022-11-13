43b7f83e8bf6b19938202dc6621971d0780e4f1c
abbypin <abby@techcleanup.com>
2022-11-13 11:57:31 -0800

    Finished Lab3-Step1
    Added Search Inputs

--- creditCard.cpp
+++ creditCard.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 using namespace std;
 
+/* Step 0. */
 // Read customer information from external file
 // Make no changes to the following code
 void ReadCustomerData(vector<string>& names, vector<string>& states, vector<double>& debt) {
@@ -31,13 +32,19 @@ void ReadCustomerData(vector<string>& names, vector<string>& states, vector<doub
 }//end ReadCustomerData()
 
 int main() {
+    // Step 0 Variable
     int numOfCustomers = 0;
+    // Step 1 Variables
+    int debtLimit = 0;
+    char firstLetterOfCustomerName;
+    string stateAbbrev;
+
     // Input # of customers and create parallel vectors
     /* Type your code */
-    
     // Get Requested Number of Customers
-    cout << "How many customers are there?" << endl;
+    cout << "How many customers are there? ";
     cin >> numOfCustomers;
+
     // Create Parallel Vectors
     vector<string> names(numOfCustomers);
     vector<string> states(numOfCustomers);
@@ -48,8 +55,26 @@ int main() {
     // Read File Data into the Vectors
     ReadCustomerData(names, states, debt);
 
-   // Input debt limit, search phrase, and state
+    /* Step 1. */
+    // Input debt limit, search phrase, and state
+    // Get the Debt Limit
+    cout << "Enter the debt limit: ";
+    cin >> debtLimit;
+    // Get a Letter to Search with
+    cout << "Enter a first initial to search with: ";
+    cin.ignore();
+    cin.get(firstLetterOfCustomerName);
+    // Get a State Abbreviation
+    cout << "Enter a State Abbreviation: ";
+    cin.ignore();
+    getline(cin, stateAbbrev);
 
+    // Test Code to Check that Step 0 and Step 1 Works
+    // cout << "\nTest Inputs: " << endl;
+    // cout << "Number of Customers: " << numOfCustomers << endl;
+    // cout << "Debt Limit: " << debtLimit << endl;
+    // cout << "First Initial Letter: " << firstLetterOfCustomerName << endl;
+    // cout << "State Abbreviation: " << stateAbbrev << endl;
 
 
    return 0;
