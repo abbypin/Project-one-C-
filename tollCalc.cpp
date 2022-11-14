035f58b98dacacb3d8eb881d3461135a46206598
abbypin <abby@techcleanup.com>
2022-11-13 20:24:41 -0800

    Update Lab1

--- tollCalc.cpp
+++ tollCalc.cpp
@@ -1,32 +1,33 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
 using namespace std;
 
 double CalcToll(int currentHour, bool morning, bool weekend) {
     double toll = 0.00;
-    if (!(weekend) && (morning) && (currentHour < 7 || currentHour == 12)) {
+    if (((weekend == 0) && (morning == 1)) && (currentHour < 7 || currentHour == 12)) {
         toll = 1.15;
-    } else if (!(weekend) && (morning) && (currentHour >= 7 && currentHour < 10)) {
+    } else if (((weekend == 0) && (morning == 1)) && (currentHour >= 7 && currentHour < 10)) {
         toll = 2.95;
-    } else if (!(weekend) && (morning) && (currentHour >= 10 && currentHour < 12)) {
+    } else if (((weekend == 0) && (morning == 1)) && (currentHour >= 10 && currentHour < 12)) {
         toll = 1.90;
-    } else if ((!weekend && !morning) && (currentHour >= 12 && currentHour < 3)) {
+    } else if (((weekend == 0) && (morning == 0)) && (currentHour <= 12 && currentHour < 3)) {
         toll = 1.90;
-    } else if ((!weekend && !morning) && (currentHour >= 3 && currentHour < 8)) {
+    } else if (((weekend == 0) && (morning == 0)) && (currentHour >= 3 && currentHour < 8)) {
         toll = 3.95;
-    } else if ((!weekend && !morning) && (currentHour >= 8)) {
+    } else if (((weekend == 0) && (morning == 0)) && (currentHour >= 8)) {
         toll = 1.40;
     } else {
         toll = 0.00;
     }//end else-if
 
-    if ((weekend && morning) && (currentHour < 7 || currentHour == 12)) {
+    if (((weekend == 1) && (morning == 1)) && (currentHour < 7 || currentHour == 12)) {
         toll = 1.05;
-    } else if ((weekend && morning) && (currentHour >= 7 && currentHour < 12)) {
+    } else if (((weekend == 1) && (morning == 1)) && (currentHour >= 7 && currentHour < 12)) {
         toll = 1.05;
-    } else if ((weekend && !morning) && (currentHour >= 12 && currentHour < 8)) {
+    } else if (((weekend == 1) && (morning == 0)) && (currentHour <= 12 && currentHour < 8)) {
         toll = 2.15;
-    } else if ((weekend && !morning) && (currentHour >= 8)) {
+    } else if (((weekend == 1) && (morning == 0)) && (currentHour >= 8)) {
         toll = 1.10;
     } else {
         toll = 0.00;
@@ -36,9 +37,9 @@ double CalcToll(int currentHour, bool morning, bool weekend) {
 }//end CalcToll()
 
 int main() {
-    bool isMorning;
+    bool isMorning = 0;
     string morning;
-    bool isWeekend;
+    bool isWeekend = 0;
     string weekend;
     string time;
     string hour;
@@ -48,23 +49,21 @@ int main() {
     cout << "Is it Morning? (yes or no):" << endl;
     cin >> morning;
 
-    if (morning.compare("yes") || morning.compare("Yes")) {
-        isMorning = true;
-    } else if (morning.compare("no") || morning.compare("No")) {
-        isMorning = false;
-    } else {
-        isMorning = false;
+    if (morning.compare("yes") == 0 || morning.compare("Yes") == 0) {
+        isMorning = 1;
+    }
+    if (morning.compare("no") == 0 || morning.compare("No") == 0) {
+        isMorning = 0;
     }//end else-if
 
     cout << "Is it the Weekend? (yes or no):" << endl;
     cin >> weekend;
 
-    if (weekend.compare("yes") || weekend.compare("Yes")) {
-        isWeekend = true;
-    } else if (weekend.compare("no") || weekend.compare("No")) {
-        isWeekend = false;
-    } else {
-        isWeekend = false;
+    if (weekend.compare("yes") == 0 || weekend.compare("Yes") == 0) {
+        isWeekend = 1;
+    }
+    if (weekend.compare("no") == 0 || weekend.compare("No") == 0) {
+        isWeekend = 0;
     }//end else-if
 
     cout << "What time is it?" << endl;
@@ -73,7 +72,7 @@ int main() {
     hour = time.substr(0, index);
     currentHour = stoi(hour);
 
-    cout << "Toll Amount: " << CalcToll(currentHour, isMorning, isWeekend) << endl;
+    cout << "Toll Amount: " << fixed << setprecision(2) << CalcToll(currentHour, isMorning, isWeekend) << endl;
 
     return 0;
 }//end main()
